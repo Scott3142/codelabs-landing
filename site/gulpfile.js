17d99d4d67ac5e6c9296946fbb193abc9c8af0cd
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-14 11:38:42 +0100

    Trying new way of deploying

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -946,19 +946,19 @@ gulp.task('publish:prod:views', (callback) => {
 Deploy distribution site to Github pages
 */
 // Creates a folder called dist and adds the minified website to it.
-gulp.task('create:dist', gulp.series('dist', () => {
-  return gulp.src('dist')
-}));
+//gulp.task('create:dist', gulp.series('dist', () => {
+//  return gulp.src('dist')
+//}));
 
 // Deploys production website to gh-pages branch
-var deploy = require('gulp-gh-pages');
-gulp.task('deploy', gulp.series(
-  'create:dist',
-  function () {
-    return gulp.src("./dist/**/*")
-      .pipe(deploy({
-        remoteUrl: "https://github.com/Scott3142/codelabs-landing.git",
-        branch: "gh-pages"
-      }))
-  }
-));
+//var deploy = require('gulp-gh-pages');
+//gulp.task('deploy', gulp.series(
+//  'create:dist',
+//  function () {
+//    return gulp.src("./dist/**/*")
+//     .pipe(deploy({
+//        remoteUrl: "https://github.com/Scott3142/codelabs-landing.git",
+//        branch: "gh-pages"
+//      }))
+//  }
+//));
