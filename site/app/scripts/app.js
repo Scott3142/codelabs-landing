17b6bd9aaa893a2737bd0b376f9c3ccb90a61d41
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-14 13:58:50 +0100

    Rel links work, fixing image links now

--- site/app/scripts/app.js
+++ site/app/scripts/app.js
@@ -234,7 +234,7 @@
     } else {
       let script = document.createElement('script');
       script.async = true;
-      script.src = '/bower_components/webcomponentsjs/webcomponents-lite.min.js';
+      script.src = 'https://scott3142.uk/codelabs-landing/bower_components/webcomponentsjs/webcomponents-lite.min.js';
       document.head.appendChild(script);
     }
   }
